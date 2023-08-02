acbd691bcb0de5498889e2cdb9b2dd786dd53709
KrutosVIP <52695559+KrutosVIP@users.noreply.github.com>
2023-08-02 16:58:37 +0000

    Revert "ANDROID: drivers: Import KernelSU"
    This reverts commit 7c1d3af6173cc9522b497add4d5b11990225d56b.

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,13 +2,15 @@
 set -e
 
 [ ! -e "scripts/packaging/pack.sh" ] && git submodule init && git submodule update
-[ ! -d "toolchain" ] && echo "Make toolchain avaliable at $(pwd)/toolchain" && exit
+[ ! -e "toolchain" ] && echo "Make toolchain avaliable at $(pwd)/toolchain" && exit
 
 export KBUILD_BUILD_USER=Royna
 export KBUILD_BUILD_HOST=GrassLand
 
 PATH=$PWD/toolchain/bin:$PATH
 
+TARGET=aosp
+FLAGS=
 if [ "$1" = "oneui" ]; then
 FLAGS=ONEUI=1
 else
@@ -18,7 +20,6 @@ fi
 if [ "$2" = "ksu" ]; then
 CONFIG_KSU=vendor/ksu.config
 fi
-
 if [ -z "$DEVICE" ]; then
 export DEVICE=a51
 fi
@@ -27,5 +28,6 @@ rm -rf out
 make O=out CROSS_COMPILE=aarch64-linux-gnu- CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar \
 	OBJDUMP=llvm-objdump READELF=llvm-readelf -j$(nproc) \
 	vendor/${DEVICE}_defconfig vendor/grass.config vendor/${DEVICE}.config $CONFIG_AOSP $CONFIG_KSU
+
 make O=out CROSS_COMPILE=aarch64-linux-gnu- CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar \
 	OBJDUMP=llvm-objdump READELF=llvm-readelf ${FLAGS} -j$(nproc)
